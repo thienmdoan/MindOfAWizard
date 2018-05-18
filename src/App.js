268936e55eb0ebafb0c40e2ae4ae693e27cea2ae
Thien Doan <thien@clarifymed.com>
2018-05-17 22:39:09 -0700

    Updating app to inclue proper title.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Mind of a Wizard',
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome the {this.state.name}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
